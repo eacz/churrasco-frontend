85308858da7dd3d7cf142a27f439d5f66496e9d5
Esteban Canteros <estebanacanteros@gmail.com>
2022-05-16 14:26:16 -0300

    variables de entorno hardcodeadas

--- src/shared/axiosInstance.js
+++ src/shared/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create({ baseURL: process.env.VUE_APP_BACKEND_URL })
+const axiosInstance = axios.create({ baseURL: 'http://localhost:4000' })
 
 export default axiosInstance
